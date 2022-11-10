2068bf4f9e3c860910614c8a55e55b24029f7e10
Andy <AndyStubbs@users.noreply.github.com>
2022-11-10 03:31:31 +0000

    Fixed bug with vegas score accumulator.

--- src/sol.js
+++ src/sol.js
@@ -350,8 +350,8 @@ let g_sol = ( function () {
 		}
 		let elapsed = calcElapsedTime() / 1000;
 		if( m_scoreMode === "Vegas" ) {
-			m_score -= g_menu.getVegasStartScore();
 			g_menu.setVegasStartScore( m_score );
+			m_score -= g_menu.getVegasStartScore();
 		}
 		gameStats.push( {
 			"date": ( new Date() ).getTime(),
